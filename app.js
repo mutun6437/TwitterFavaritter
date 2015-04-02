b28e25cee3a634105d9792574422355a8fbd6868
mutun6437 <higenjitukyoukai@gmail.com>
2015-04-02 23:46:02 +0900

    first commit

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ var users = require('./routes/users');
 
 var app = express();
 
+var http = require('http');
+
+var socketio = require("socket.io");
+
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -57,15 +64,68 @@ app.use(function(err, req, res, next) {
 });
 
 
+
+app.post("/post",function(req,res){
+    console.log(req.body);
+    res.redirect("/");
+});
+
+
 module.exports = app;
 
-
-
-var server = app.listen(3000, function () {
+server.listen(3000, function () {
 
   var host = server.address().address
   var port = server.address().port
 
   console.log('Example app listening at http://%s:%s', host, port)
 
-})
+});
+
+
+
+
+
+var util = require('util'),
+    twitter = require('twitter');
+
+
+var twit = new twitter({
+    consumer_key: 'Z8Gv0AR5d44ZqxWyECvcAg',
+    consumer_secret: 'CGaYTPgBmNApy27Xo1AdPRExMv3fRYzwkl2qXF9GM',
+    access_token_key: '1882925552-xGr8Pg0QBU59qRSxC6BwJELMMNmiHeYF9frSyxg',
+    access_token_secret: '6wdThoKqzJwuBLZvNUnBnq52Sx808X8owr4hNe6a5g8jk'
+});
+
+/*
+twit.get('search/tweets', {q: '',count:10}, function(error, tweets, response){
+  //console.log(tweets);
+  
+    var tweet = tweets.statuses;  
+    var randomTweet = randIndex(tweet); 
+    
+    twit.post('favorites/create', { id: randomTweet.id_str }, function(err){console.log(err)});
+    
+});
+*/
+
+
+
+
+function randIndex (arr) {
+  var index = Math.floor(arr.length*Math.random());
+  return arr[index];
+};
+
+
+io.sockets.on("connection", function (socket) {
+    socket.on("search",function(searchText){
+        //console.log(data);
+        twit.get('search/tweets', {q: searchText,count:10}, function(error, tweets, response){
+            var tweet = tweets.statuses;  
+            var randomTweet = randIndex(tweet); 
+            twit.post('favorites/create', { id: randomTweet.id_str }, function(err){console.log(err)});
+        });
+
+    });
+});
